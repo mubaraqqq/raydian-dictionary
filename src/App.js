8bc8e78e8e192efc3a3832e68a2d64a4a2863a81
Mubaraqqq <ayomidemusty@gmail.com>
2022-04-27 01:59:24 +0100

    Implemented the random word on loading/reloading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import WordProfile from './components/WordProfile';
+import { Card, CardContent, Typography } from '@mui/material';
 
 function App() {
+  const [random, setRandom] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://random-words-api.vercel.app/word')
+      .then((res) => {
+        setRandom(res.data[0].word);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+  console.log(random)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        random && <WordProfile word={random} />
+      }
     </div>
   );
 }
---
+++ src/components/WordProfile.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, CardContent, Typography } from '@mui/material';
+
+
+
+const WordProfile = ({ word }) => {
+  const [wordData, setWordData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const firstLetterUpperCase = (arr) => {
+        if (arr) {
+            let str = arr?.split('');
+            str[0] = str[0].toUpperCase();
+            return str.join('');
+        }
+    };
+
+
+    
+  useEffect(() => {
+      axios
+        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then((res) => {
+            setWordData(res.data[0]);
+            setIsLoading(false);
+        })
+        .catch((err) => {
+            console.log(err);
+            setIsLoading(false);
+        })
+  }, [word])
+
+  console.log(wordData);
+  console.log(isLoading);
+  
+  if (isLoading) return <h1>Loading</h1>
+    
+  return (
+    <Card sx={{ width: 700 }}>
+    <CardContent>
+        <Typography variant="h5" component="div">
+            {firstLetterUpperCase(wordData.word)}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {wordData.meanings[0].partOfSpeech}
+        </Typography>
+        <Typography variant="body2">
+            {wordData.meanings[0].definitions[0].definition}
+            <br />
+            {'"a benevolent smile"'}
+        </Typography>
+    </CardContent>
+    </Card>
+  )
+}
+
+export default WordProfile
